4faaf2f3dc2e08904bc6e764d8adec0c6793778c
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2019-08-17 18:16:57 -0700

    barghi plots

---
+++ notebooks/plots/barghi_combined.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import matplotlib as mpl
+
+temp_covs_bs = np.load("../../data/barghi_et_al_2019/temp_covs_bootstrap.npy")
+g_cis = np.load("../../data/barghi_et_al_2019/G_bootstrap.npy")

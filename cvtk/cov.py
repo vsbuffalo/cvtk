new figures, and bug fix with covariance calculation option

cvtk/cov.py
@@ -257,6 +257,7 @@ def stack_replicate_covs_by_group(covs, R, T, stack=True, **kwargs):
 def temporal_replicate_cov(freqs, depths=None, diploids=None,
                            bias_correction=True, standardize=True, deltas=None,
                            use_masked=False, share_first=False,
+                           product_only=False,
                            return_ratio_parts=False, warn=False):
     """
     Params:
@@ -304,9 +305,15 @@ def temporal_replicate_cov(freqs, depths=None, diploids=None,
     # calculate variance-covariance matrix
     if use_masked:
         deltas_masked = np.ma.masked_invalid(deltas)
-        cov = np.ma.cov(deltas_masked, bias=True).data
+        if product_only:
+            cov = np.ma.dot(deltas_masked, deltas_masked.T) / L
+        else:
+            cov = np.ma.cov(deltas_masked, bias=True).data
     else:
-        cov = np.cov(deltas, bias=True)
+        if product_only:
+            cov = deltas @ deltas.T / L
+        else:
+            cov = np.cov(deltas, bias=True)
 
     if not bias_correction:
         if return_ratio_parts:

cvtk/cvtk.py
@@ -92,7 +92,8 @@ class TemporalFreqs(object):
     def R(self):
         return self.freqs.shape[0]
 
-    def calc_cov(self, keep_seqids=None, bias_correction=True, standardize=True, use_masked=False):
+    def calc_cov(self, keep_seqids=None, bias_correction=True,
+                 standardize=True, use_masked=False, product_only=False):
         """
         Calculate the genome-wide temporal-replicate variance-covariance matrix.
 
@@ -108,6 +109,7 @@ class TemporalFreqs(object):
         return temporal_replicate_cov(freqs, depths, self.diploids,
                                       bias_correction=bias_correction,
                                       share_first=self.share_first,
+                                      product_only=product_only,
                                       standardize=standardize, use_masked=use_masked)
 
     def convergence_corr(self, subset=None, bias_correction=True, **kwargs):
@@ -159,8 +161,12 @@ class TemporalFreqs(object):
         return var_by_group(groups, freqs=self.freqs, depths=self.depths, diploids=self.diploids,
                             t=t, standardize=standardize, bias_correction=bias_correction)
 
-    def calc_G(self, average_replicates=False, abs=False, use_masked=False):
-        covs = self.calc_cov(standardize=False, use_masked=use_masked)
+    def calc_G(self, average_replicates=False, abs=False,
+               product_only=False,
+               use_masked=False):
+        covs = self.calc_cov(standardize=False,
+                             product_only=product_only,
+                             use_masked=use_masked)
         # calculate the total variances for different ts
         vars = []
         for t in np.arange(1, self.T+1):
@@ -422,6 +428,7 @@ class TiledTemporalFreqs(TemporalFreqs):
     def calc_empirical_null(self, B=100, exclude_seqs=None,
                        sign_permute_blocks='tile',
                        by_tile=False,
+                       use_masked=False,
                        bias_correction=False, progress_bar=False):
 
         return calc_covs_empirical_null(self.freqs,
@@ -431,6 +438,7 @@ class TiledTemporalFreqs(TemporalFreqs):
                                         gintervals=self.gintervals,
                                         B=B,
                                         depths=self.depths,
+                                        use_masked=use_masked,
                                         diploids=self.diploids,
                                         by_tile=by_tile,
                                         exclude_seqids=exclude_seqs,

cvtk/empirical_null.py
@@ -19,7 +19,7 @@ def slice_loci(seqids, loci_indices=None, exclude_seqids=None):
         idx = [i for i, seqid in enumerate(seqids) if seqid not in exclude_seqids]
     else:
         loci_indices = set(loci_indices)
-        idx = [i for i, seqid in enumerate(seqids) if seqid not in exclude_seqids and 
+        idx = [i for i, seqid in enumerate(seqids) if seqid not in exclude_seqids and
                 i in loci_indices]
     #loci_slice = np.array(idx)
     return sliceify(idx)
@@ -56,13 +56,14 @@ def reshape_empirical_null(empnull, R, T):
     return np.stack(empcovs)
 
 
-def calc_covs_empirical_null(freqs, tile_indices, tile_seqids, 
-                             tile_ids, gintervals, 
-                             B=100, 
+def calc_covs_empirical_null(freqs, tile_indices, tile_seqids,
+                             tile_ids, gintervals,
+                             B=100,
                              depths=None, diploids=None,
                              by_tile=False,
-                             exclude_seqids=None, 
-                             sign_permute_blocks='tile', bias_correction=False, 
+                             use_masked=False,
+                             exclude_seqids=None,
+                             sign_permute_blocks='tile', bias_correction=False,
                              progress_bar=True):
     """
     Params:
@@ -101,7 +102,7 @@ def calc_covs_empirical_null(freqs, tile_indices, tile_seqids,
         B_range = tnrange(int(B))
     else:
         B_range = range(int(B))
-    
+
     # main delta permutation procedure
     all_covs = list()
     for b in B_range:
@@ -119,17 +120,18 @@ def calc_covs_empirical_null(freqs, tile_indices, tile_seqids,
             raise ValueError("sign_permute_blocks must be either 'tile' or 'seqid'")
 
         if by_tile:
-            covs = cov_by_group(tile_indices, sliced_freqs, 
+            covs = cov_by_group(tile_indices, sliced_freqs,
                                  depths=sliced_depths,
                                  diploids=sliced_diploids,
-                                 bias_correction=bias_correction, 
-                                 deltas=permuted_deltas)
+                                 bias_correction=bias_correction,
+                                 deltas=permuted_deltas, use_masked=use_masked)
         else:
             covs = temporal_replicate_cov(sliced_freqs, depths=sliced_depths,
                                           diploids=sliced_diploids,
                                           bias_correction=bias_correction,
-                                          deltas=permuted_deltas)
-          
+                                          deltas=permuted_deltas,
+                                          use_masked=use_masked)
+
         all_covs.append(covs)
     if by_tile:
         return reshape_empirical_null(all_covs, R, T)